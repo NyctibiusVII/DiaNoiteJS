df8a044c525e373ef1706c5463a1eed53db5b9fc
NyctibiusVII <matheus.dev.07@gmail.com>
2020-08-27 23:58:01 -0300

    adding condition structures

--- script.js
+++ script.js
@@ -4,5 +4,24 @@ function carregar(){
     var data = new Date()
     var min = data.getMinutes()
     var hora = data.getHours()
+    //var hora = 13 //para teste
     msg.innerHTML = `Agora são ${hora} horas e ${min} minutos`
+
+    if(hora >= 6 && hora < 12){
+        //dia
+      document.body.style.background = '#fff56a'
+      imgH.src = 'img/manha.jpg'
+    }else if(hora >= 12 && hora < 18){
+        //tarde
+      document.body.style.background = '#ff9e1fda'
+      imgH.src = 'img/tarde.jpg'    
+    }else if(hora >= 18 && hora < 23){
+        //noite
+      document.body.style.background = '#1c305a'
+      imgH.src = 'img/noite.jpg'    
+    }else{
+        //madrugada
+      document.body.style.background = '#000714'
+      imgH.src = 'img/madrugada.jpg' 
+    }
 }  
